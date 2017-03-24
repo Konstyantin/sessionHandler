48ba83dff2e6b5ee79b59471e21b066ad2f40ce2
Konstantin <kostyannagula@gmail.com>
2017-03-24 11:26:01 +0200

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 23.03.2017
+ * Time: 17:30
+ */
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+require_once __DIR__ . '/vendor/autoload.php';
---
+++ public/assert/js/script.js
@@ -0,0 +1,3 @@
+/**
+ * Created by User on 24.03.2017.
+ */
